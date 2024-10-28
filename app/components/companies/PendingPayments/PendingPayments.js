c5e8133fc4ff749d64d41fb12cd7259520a2a145
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2024-10-28 15:38:27 +0530

    Added a comment

--- app/components/companies/PendingPayments/PendingPayments.js
+++ app/components/companies/PendingPayments/PendingPayments.js
@@ -3,6 +3,7 @@ import Company from "./Company"
 import styles from "./PendingPayments.module.css"
 
 const PendingPayments = () => {
+  //pending payments request to backend
   return (
     <div className={styles.pendingPayments}>
       <h2>PENDING PAYMENTS</h2>
